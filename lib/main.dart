ebe8188cbb20c9ef906d23aa4e65d1b46aa585de
bEinG_RudY <pawansinghfea3@gmail.com>
2023-01-08 15:51:42 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -33,33 +33,75 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: HexColor("#464646"),
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: HexColor("#000000"),
-        title: Text("HEADLINES",
-            style: GoogleFonts.robotoSlab(
-              textStyle: TextStyle(
-                fontSize: 29,
-                color: HexColor("#ffffff"),
-              ),
-            )),
-      ),
-      body:
-      Column(
-        children: [
-          Container(
-            child: ListView.builder(
-              shrinkWrap: true,
-                itemCount: 3,
-                itemBuilder: (context, index){
-                  return Container(
-                    child:Text("This is news")
-                  );
-                })
+        backgroundColor: HexColor("#464646"),
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: HexColor("#000000"),
+          title: Text("HEADLINES",
+              style: GoogleFonts.robotoSlab(
+                textStyle: TextStyle(
+                  fontSize: 29,
+                  color: HexColor("#ffffff"),
+                ),
+              )),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                  child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: 6,
+                      itemBuilder: (context, index) {
+                        return Container(
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 12),
+                            child: Card(
+                              shadowColor: Colors.black,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15)),
+                              elevation: 1.5,
+                              child: Stack(
+                                children: [
+                                  ClipRRect(
+                                      borderRadius: BorderRadius.circular(15),
+                                      child: Image.asset(
+                                          "assets/images/dead1.jpg")),
+                                  Positioned(
+                                      left: 0,
+                                      right: 0,
+                                      bottom: 0,
+                                      child: Container(
+                                          height: 160,
+                                          decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(15),
+                                              gradient: LinearGradient(
+                                                  colors: [
+                                                    Colors.black12
+                                                        .withOpacity(0),
+                                                    Colors.black
+                                                  ],
+                                                  begin: Alignment.topCenter,
+                                                  end: Alignment.bottomCenter)),
+                                          padding: EdgeInsets.fromLTRB(
+                                              10, 15, 10, 5),
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                "News headline",
+                                              ),
+                                              Text("News Description")
+                                            ],
+                                          )))
+                                ],
+                              ),
+                            ));
+                      })),
+            ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
